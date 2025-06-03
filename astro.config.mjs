50078288c99473b72d125de0ce3380451e833db1
Christian Köberl <christian.koeberl@porscheinformatik.com>
2025-06-03 13:44:29 +0200

    add weltauto detail

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
